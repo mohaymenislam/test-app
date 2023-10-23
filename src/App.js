9ecb035d34a65698bdcc356d34a3475436d13df6
mohaymenislam <mohaymenislam412@gmail.com>
2023-10-23 00:05:10 -0700

    second commit added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hello Bangladesh
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hello world
-        </a>
-      </header>
-    </div>
-  );
+    <>
+       <Navbar></Navbar>
+    </>
+  )
 }
 
-export default App;
+export default App
